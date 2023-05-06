d34127103b8410fcf6830bbfa41eefd77c351ce6
Aditya Ghaffar <adit.bisa@gmail.com>
2023-05-07 06:03:38 +0700

    refactor: use path alias

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MariadbConfigModule, MariadbConfigService } from './configs/mariadb';
-import { HealthModule } from './controllers/health';
+import { MariadbConfigModule, MariadbConfigService } from '@configs/mariadb';
+import { HealthModule } from '@controllers/health';
 
 @Module({
   imports: [
