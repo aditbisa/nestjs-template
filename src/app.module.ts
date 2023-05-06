f23e444eedcacac7f58c748da05c8caee5e81e7e
Aditya Ghaffar <adit.bisa@gmail.com>
2023-05-07 03:15:03 +0700

    feat: register database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MariadbConfigModule, MariadbConfigService } from './configs/mariadb';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [MariadbConfigModule],
+      useFactory: (config: MariadbConfigService) => ({
+        type: 'mariadb',
+        connectorPackage: 'mysql2',
+        host: config.host,
+        port: config.port,
+        username: config.user,
+        password: config.password,
+        database: config.database,
+        entities: [],
+        synchronize: true, // remove on prod and use migration
+        timezone: '+07:00', // Jakarta
+      }),
+      inject: [MariadbConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
